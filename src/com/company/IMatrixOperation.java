9514b4a793664193c05bbe00c4c44dd12f04e71c
dobosizsolt <dobosi.zsolt@gmail.com>
2017-12-14 20:43:08 +0100

    #2

---
+++ src/com/company/IMatrixOperation.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public interface IMatrixOperation {
+    void printMatrix(int[][] matrix);
+}
---
+++ src/com/company/MatrixOperation.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class MatrixOperation implements IMatrixOperation {
+
+    public void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
