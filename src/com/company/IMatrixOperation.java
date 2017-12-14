8e3789307b34ce078021fac30e63fc6e13d43d3d
dobosizsolt <dobosi.zsolt@gmail.com>
2017-12-14 21:06:52 +0100

    #3

--- src/com/company/IMatrixOperation.java
+++ src/com/company/IMatrixOperation.java
@@ -2,4 +2,6 @@ package com.company;
 
 public interface IMatrixOperation {
     void printMatrix(int[][] matrix);
+
+    int[][] initMatrixWithRandomNumbers(int n, int m, int min, int max);
 }
--- src/com/company/MatrixOperation.java
+++ src/com/company/MatrixOperation.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Random;
+
 public class MatrixOperation implements IMatrixOperation {
 
     public void printMatrix(int[][] matrix) {
@@ -9,5 +11,18 @@ public class MatrixOperation implements IMatrixOperation {
             }
             System.out.println();
         }
+    }
+
+    public int[][] initMatrixWithRandomNumbers(int n, int m, int min, int max) {
+
+        Random random = new Random();
+        int[][] matrix = new int[n][m];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = random.nextInt(max - min + 1) + min;
+            }
+        }
+        return matrix;
     }
 }
