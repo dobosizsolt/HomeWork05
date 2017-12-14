c0e02cbe667ad83a25b2ed44da62ee5cf0594a88
dobosizsolt <dobosi.zsolt@gmail.com>
2017-12-14 21:22:30 +0100

    #4

--- src/com/company/Sort.java
+++ src/com/company/Sort.java
@@ -4,14 +4,22 @@ public class Sort implements ISort {
 
     public void bubbleSort(int[] array) {
         for (int i = array.length - 1; i > 0; i--) {
+
+            int swaps = 0;
+
             for (int j = 0; j <= i - 1; j++) {
                 if (array[j] < array[j + 1]) {
                     int getNum = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = getNum;
+                    swaps++;
                 }
             }
+            if (swaps == 0) {
+                break;
+            }
         }
+
     }
 
     public void insertionSort(int[] array) {
